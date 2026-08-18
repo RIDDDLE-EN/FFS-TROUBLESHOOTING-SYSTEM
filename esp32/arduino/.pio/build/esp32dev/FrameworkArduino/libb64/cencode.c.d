c76a71aff5268cbe9e98ff11c310bf5c9d071422
RIDDDLE-EN <ridddle5050@gmail.com>
2026-08-18 09:26:30 +0300

    ignored pycache and the database files

---
+++ esp32/arduino/.pio/build/esp32dev/FrameworkArduino/libb64/cencode.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/libb64/cencode.c.o: \
+ /home/ston/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
+ /home/ston/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
